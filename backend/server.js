258bd08fdf83efa8c0ed72a8e3d611e23e6b838e
naman <naman05mah@gmail.com>
2026-01-20 16:09:13 +0530

    naman

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ console.log("First 10 chars:", process.env.OPENROUTER_API_KEY?.substring(0, 10))
 const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
+  "https://neural-sync-chi.vercel.app/"
 ];
 
 app.use(cors({ origin: allowedOrigins }));
